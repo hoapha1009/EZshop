b527b5b811f2d491384ba164a1343adc4761d5b5
HoangPham <hoapha1009@gmail.com>
2020-11-23 14:55:52 +0700

    Finish feature/activetodo

--- src/features/Todo/components/TodoList/index.jsx
+++ src/features/Todo/components/TodoList/index.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './styles.scss';
 
 TodoList.propTypes = {
     todoList: PropTypes.array,
+    onTodoClick: PropTypes.func,
 };
 
 TodoList.defaultProps = {
     todoList: [],
+    onTodoClick: null,
 };
 
-function TodoList({ todoList }) {
+function TodoList({ todoList, onTodoClick }) {
+    const handleTodoClick = (todo, idx) => {
+        if (!onTodoClick) return;
+        onTodoClick(todo, idx);
+    };
     return (
         <ul>
             {todoList.map((todo, idx) => (
-                <li key={todo.id}>{todo.title}</li>
+                <li key={todo.id} onClick={() => handleTodoClick(todo, idx)}>
+                    {todo.title}
+                </li>
             ))}
         </ul>
     );
