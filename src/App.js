9b803f683197a25b2fba5829d7fdff2876fbf01d
HoangPham <hoapha1009@gmail.com>
2020-11-23 14:22:04 +0700

    Finish feature/albumlist

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.scss';
 import TodoFeature from 'features/Todo';
+import AlbumFeature from 'features/Album';
 
 function App() {
     return (
         <div className="App">
             Todo list:
             <TodoFeature />
+            Album list:
+            <AlbumFeature />
         </div>
     );
 }
