2c2638ba734fbf5a43e376cf1f5253fb3cc8f4f5
HoangPham <hoapha1009@gmail.com>
2020-11-23 15:31:59 +0700

    Add routing for app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './App.scss';
 import TodoFeature from 'features/Todo';
 import AlbumFeature from 'features/Album';
+import { NavLink, Route, Switch } from 'react-router-dom';
 
 function App() {
     return (
         <div className="App">
-            Todo list:
-            <TodoFeature />
-            Album list:
-            <AlbumFeature />
+            <p>
+                <NavLink to="/todos" activeClassName="active-menu">
+                    Todo
+                </NavLink>
+            </p>
+            <p>
+                <NavLink to="/albums" activeClassName="active-menu">
+                    Album
+                </NavLink>
+            </p>
+            <Switch>
+                <Route path="/todos" component={TodoFeature} />
+                <Route path="/albums" component={AlbumFeature} />
+            </Switch>
         </div>
     );
 }
